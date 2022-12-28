af83238aae01119405cb4584084a0a710711a048
Steven <srammos1@gmail.com>
2022-12-28 01:19:13 +0000

    Saved progress on ReturnArrayContents 2022-12-28 01:19:13 +0000

--- ReturnArrayContents/src/main/java/ReturnArrayContents.java
+++ ReturnArrayContents/src/main/java/ReturnArrayContents.java
@@ -28,7 +28,7 @@ public class ReturnArrayContents {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        //String str = "";
+        //can do this or below --> String str = "";
         String str = new String();
         for(int i = 0; i < nums.length; i++){
             
