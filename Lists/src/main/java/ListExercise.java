a4d463c81a8933dfd50ee9a3b349a415324e7674
Steven <srammos1@gmail.com>
2022-12-29 20:17:14 +0000

    Saved progress on Lists 2022-12-29 20:17:14 +0000

--- Lists/src/main/java/ListExercise.java
+++ Lists/src/main/java/ListExercise.java
@@ -91,7 +91,7 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+        list.remove(position);
     }
 
     /**
@@ -104,5 +104,6 @@ public class ListExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void updateAtPosition(List<Integer> list, int position, int value){
+        
     }
 }
