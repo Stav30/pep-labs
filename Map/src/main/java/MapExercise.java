12cd12481916ddd0a05d4c6d7a285a64ccd50b58
Steven <srammos1@gmail.com>
2022-12-30 02:46:43 +0000

    Saved progress on Map 2022-12-30 02:46:43 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -73,6 +73,8 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
+    
     }
 
     /**
@@ -83,6 +85,6 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        map.put(key,value);
     }
 }
