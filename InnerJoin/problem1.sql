2c812092813cc913e9cc2c1f1283096f348e4465
Steven <srammos1@gmail.com>
2023-01-17 03:06:05 +0000

    Saved progress on InnerJoin 2023-01-17 03:06:05 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,2 +1,4 @@
-SELECT student.id , student.student_name FROM class.id INNER JOIN student.class_title
+SELECT student.id , student.student_name 
+FROM class INNER JOIN student
+ON class.id = student.class_title
 WHERE class.id = 1 || 2;
