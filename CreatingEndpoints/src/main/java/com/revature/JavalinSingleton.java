57ae2d63780b5970614428f3b8eb6e3382bb179d
Steven <srammos1@gmail.com>
2023-01-17 00:01:03 +0000

    Saved progress on CreatingEndpoints 2023-01-17 00:01:03 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -12,9 +12,14 @@ public class JavalinSingleton {
      * Note: Please refer to the "CreatingEndpoints.MD" file for more assistance if needed.
      */
     public static Javalin getInstance(){
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create().start(9000);
         
         //write endpoint here
+         // HTTP POST REQUEST to http://localhost:9000/post-request
+    app.post("/post-request", ctx -> {
+        //logic to be executed when this endpoint is hit
+        ctx.result("Hello World");
+    });
 
         return app;
     }
