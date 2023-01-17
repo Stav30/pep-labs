abcfb51dd4651dfddd53719297b7ae15931de085
Steven <srammos1@gmail.com>
2023-01-17 18:54:49 +0000

    Saved progress on InnerJoin 2023-01-17 18:54:49 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,4 +1,1 @@
-SELECT student.id , student.student_name 
-FROM class INNER JOIN student
-ON class.id = student.class_title
-WHERE class.id = 1 || 2;
+SELECT student.id , student.student_name FROM student INNER JOIN class ON student.class_title = class.class_title WHERE class.teacher_name = 'Ms. Lovelace';
