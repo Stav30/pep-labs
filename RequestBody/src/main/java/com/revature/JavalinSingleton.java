5293bbdb5d1bfab9a5f7547a4d2a08d51ff85eac
Steven <srammos1@gmail.com>
2023-01-22 02:56:07 +0000

    Saved progress on RequestBody 2023-01-22 02:56:07 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -38,8 +38,6 @@ public class JavalinSingleton {
              //return the json string in the response body
             ctx.result(jsonStringToBeReturned);
 
-
-
         });     
             
 
@@ -51,7 +49,27 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem2", ctx -> {
-               //implement logic here
+            //retrieve the json string from the request body
+            String jsonString = ctx.body();
+
+            //utilize jackson to convert the json string to a user object
+            ObjectMapper om = new ObjectMapper();
+            User user = om.readValue(jsonString, User.class);
+
+            //we need to let the request know we will send back json in the body
+            ctx.contentType("application/json"); 
+
+            //change the last name
+            user.setLastname("Jones");
+    
+            //utilize jackson convert back the user object to a json string
+            String jsonStringToBeReturned = om.writeValueAsString(user);
+
+            //return the json string in the response body
+            ctx.result(jsonStringToBeReturned);
+            
+       
+            //implement logic here
         });
 
 
