516793690f5e01d0df1b5f130adba6a80b7442f8
Steven <srammos1@gmail.com>
2023-01-25 05:12:41 +0000

    Saved progress on CombineArrays 2023-01-25 05:12:41 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class CombineArrays {
     /**
@@ -9,6 +10,24 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+         // determines length of firstArray
+         int a1 = arr1.length;
+        
+         // determines length of secondArray
+         int b1 = arr2.length;
+         
+         // resultant array size
+         int c1 = a1 + b1;
+   
+         // create the resultant array
+         int[] c = new int[c1];
+   
+         // using the pre-defined function arraycopy
+         System.arraycopy(arr1, 0, c, 0, a1);
+         System.arraycopy(arr2, 0, c, a1, b1);
+        
+         return c;
+       
+        //return null;
     }
 }
