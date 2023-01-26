ae060f836c2165a194940e31b288d78708e565e7
Steven <srammos1@gmail.com>
2023-01-26 03:12:27 +0000

    Saved progress on IsThereSum 2023-01-26 03:12:27 +0000

--- IsThereSum/src/main/java/IsThereSum.java
+++ IsThereSum/src/main/java/IsThereSum.java
@@ -12,7 +12,7 @@ public class IsThereSum {
     public boolean check(int[] arr, int target){
         for (int i = 0; i < (arr.length - 1); i++) {
             for (int j = (i + 1); j < arr.length; j++) {
-                if (arr[i] + arr[j] == x) {
+                if (arr[i] + arr[j] == target) {
                     return true;
                 }
             }
