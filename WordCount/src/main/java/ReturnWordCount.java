f1cac2232b1d99a1eb0fffaeca57402f77b624dd
Steven <srammos1@gmail.com>
2023-01-26 03:42:04 +0000

    Saved progress on WordCount 2023-01-26 03:42:04 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -6,7 +6,23 @@ public class ReturnWordCount {
      */
     public int count(String in){
         int wordCount = 0;
-        
+        boolean isWord = false; 
+        int endOfLine = in.length() - 1; 
+        char[] characters = in.toCharArray();
+
+        for (int i = 0; i < characters.length; i++) {
+            // if the char is a letter, word = true.
+            if (Character.isLetter(characters[i]) && i != endOfLine) { isWord = true;
+
+          // if char isn't a letter and there have been letters before, 
+          // counter goes up. 
+        } else if (!Character.isLetter(characters[i]) && isWord) {
+            wordCount++; 
+            isWord = false;
+
+        // last word of String; if it doesn't end with a non letter, it 
+        // wouldn't count without this.
+         } else if (Character.isLetter(characters[i]) && i == endOfLine) { wordCount++; } }
         
         return wordCount;
     }
