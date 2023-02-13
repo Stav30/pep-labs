8906e6c08949a256ee3a0ea0eb5074be4e58cf71
Steven <srammos1@gmail.com>
2023-02-13 01:44:03 +0000

    Saved progress on InsertPosition 2023-02-13 01:44:03 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -16,7 +16,21 @@ public class InsertPosition {
      */
     public int searchInsert(int[] nums, int target) {
         // me: use binary search 
-        
+        int left = 0;
+        int right = nums.length - 1;
+    
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    
+        return left;
         
         
         
